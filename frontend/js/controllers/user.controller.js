3f530d9c4ec3a31857ae3d63de617ebb98713b49
Tola Olaoke <tolaoke@spartaglobal.co>
2017-04-11 10:25:33 +0100

    implement google marker on map

--- frontend/js/controllers/user.controller.js
+++ frontend/js/controllers/user.controller.js
@@ -47,6 +47,7 @@ function UserController($stateParams, UserFactory, $scope) {
     );
   };
 
+// GOOGLE PLACES --------------------------------------------------------
   $scope.$on('gmPlacesAutocomplete::placeChanged', () => {
     var location = controller.autocomplete.getPlace().geometry.location;
     controller.lat = location.lat();
@@ -57,7 +58,41 @@ function UserController($stateParams, UserFactory, $scope) {
   });
 
 
+// GOOGLE MARKER -------------------------------------------------------
+  $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+   $scope.markers = [];
+
+   var infoWindow = new google.maps.InfoWindow();
+
+   var createMarker = function (info){
+
+       var marker = new google.maps.Marker({
+           map: $scope.map,
+           position: new google.maps.LatLng(info.lat, info.long),
+           title: info.city
+       });
+  google.maps.event.addListener(marker, 'click', function(){
+           infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+           infoWindow.open($scope.map, marker);
+       });
+
+       $scope.markers.push(marker);
+
+   }
+   for (i = 0; i < cities.length; i++){
+       createMarker(cities[i]);
+   }
+
+   $scope.openInfoWindow = function(e, selectedMarker){
+       e.preventDefault();
+       google.maps.event.trigger(selectedMarker, 'click');
+   }
+
+});
+
+
+// INIT FUNCTIONS -----------------------------------------------------
   function init() {
     controller.relatives = [];
     controller.relativeOptions = ['brother', 'sister', 'mother', 'father', 'step father', 'step mother', 'grand mother', 'grand father', 'daughter', 'son', 'step brother', 'step sister'];
